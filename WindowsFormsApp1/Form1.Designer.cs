fce71f3fdcb2437727422d73c15737357d317bc6
SKyoKen <45055188+SkyoKen@users.noreply.github.com>
2019-05-28 04:16:22 +0900

    1.0
    カメラ／画像読み込み可能
    画像保存可能
    画像処理の実装：
    　+階調変換
    　+二値化（大津の手法）
    　+適応的二値化
    　+GaussianBlur
    　+MedianBlur
    　+Canny
    　+顔検出

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 47);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 113);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "カメラを開く";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(407, 240);
+            this.pictureBoxIpl1.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(960, 540);
+            this.pictureBoxIpl1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxIpl1.TabIndex = 1;
+            this.pictureBoxIpl1.TabStop = false;
+            this.pictureBoxIpl1.Click += new System.EventHandler(this.PictureBoxIpl1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 47);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(251, 113);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "画像を開く";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(635, 47);
+            this.button3.Margin = new System.Windows.Forms.Padding(5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(251, 113);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "カメラを閉じる";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(922, 47);
+            this.button4.Margin = new System.Windows.Forms.Padding(5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(251, 113);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "画像を保存";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1226, 47);
+            this.button5.Margin = new System.Windows.Forms.Padding(5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(251, 113);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "リセット";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 33;
+            this.listBox1.Location = new System.Drawing.Point(42, 240);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 532);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1518, 812);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBoxIpl1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture cap;
+        Mat img = new Mat();
+        Mat dst = new Mat();
+        Mat res = new Mat();
+        CascadeClassifier cascade = new
+        CascadeClassifier("../../../haarcascade_frontalface_alt.xml");
+        Boolean camera = false;
+        Boolean picture = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cap = new VideoCapture(0);
+            //listbox
+            String[] msg = { "None","Gray", "Otsu","Binary", "GaussianBlur", "MedianBlur","Canny","Kao"};
+            for (int i=0;i<msg.Length;i++)listBox1.Items.Add(msg[i]);
+            listBox1.SetSelected(0, true);
+            timer1.Enabled = true;
+            button2.Enabled = true;
+            button1.Enabled = true;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+        }
+        //タイマー
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (camera) {
+                cap.Read(img);
+                res = img.Clone();
+            }
+            if (!picture&&!camera) return;
+            sentaku();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBoxIpl1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        //画像読み込み
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Jpg 画像|*.jpg|Bmp 画像|*.bmp|Png 画像|*.png ";
+            file.FilterIndex = 0;
+            file.RestoreDirectory = true;
+            file.CheckPathExists = true;
+            file.ShowDialog();
+            if (file.FileName != string.Empty)
+            {
+                try
+                {
+                    img=Cv2.ImRead(file.FileName); 
+                    res = img.Clone();
+                    picture = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            button2.Enabled = false;
+            button1.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+        //カメラを開く
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            camera = true;
+            button2.Enabled = false;
+            button1.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = false;
+        }
+        //カメラを閉じる
+        private void button3_Click(object sender, EventArgs e)
+        {
+            camera = false;
+            pictureBoxIpl1.ImageIpl = null;
+            button2.Enabled = true;
+            button1.Enabled = true;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+        }
+        //画像保存
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog file = new SaveFileDialog();
+            file.Filter = "Jpg 画像|*.jpg|Bmp 画像|*.bmp|Png 画像|*.png ";
+            file.FilterIndex = 0;
+            file.RestoreDirectory = true;
+            file.CheckPathExists = true;
+            file.FileName = System.DateTime.Now.ToString("yyyyMMddHHmmss");//保存ファイル名
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxIpl1.Image.Save(file.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);//保存
+                MessageBox.Show(this, "保存完了！", "メッセージ");
+            }
+        }
+        //リセット
+        private void button5_Click(object sender, EventArgs e)
+        {
+            picture = false;
+            pictureBoxIpl1.ImageIpl = null;
+            button2.Enabled = true;
+            button1.Enabled = true;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+        }
+        private void sentaku()
+        {
+            String mode = listBox1.Text.Trim().ToString();
+            switch (mode)
+            {
+                case "Gray":
+                    Gray();
+                    break;
+                case "Otsu":
+                    Otsu();
+                    break;
+                case "Binary":
+                    Binary();
+                    break;
+                case "GaussianBlur":
+                    GaussianBlur();
+                    break;
+                case "MedianBlur":
+                    MedianBlur();
+                    break;
+                case "Canny":
+                    Canny();
+                    break;
+                case "Kao":
+                    Kao();
+                    break;
+                default:
+                    res = img.Clone();
+                    break;
+            }
+            
+            pictureBoxIpl1.ImageIpl = res;
+        }
+        //gray
+        private void Gray()
+        {
+            Byte[] lut = new Byte[256];
+            for (int i = 0; i < 256; i++)
+            { //ルックアップテーブルの設定                
+                lut[i] = (Byte)(i >= 128 ? 255 : 0); //入力が128以上なら出力を255に、未 満なら0に   
+            }
+            Cv2.CvtColor(img, res, ColorConversionCodes.RGB2GRAY);
+        }
+        //二値化（大津の手法） 
+        private void Otsu()
+        {
+            Gray();
+            dst = res.Clone();
+            Cv2.Threshold(dst, res, 128, 255, ThresholdTypes.Otsu);
+        }
+        //適応的二値化
+        private void Binary()
+        {
+            Gray();
+            dst = res.Clone();
+            Cv2.AdaptiveThreshold(dst, res, 255, AdaptiveThresholdTypes.GaussianC, ThresholdTypes.Binary, 9, 12);
+        }
+        //Gaussian フィルタ 
+        private void GaussianBlur()
+        {
+            OpenCvSharp.Size ksize = new OpenCvSharp.Size(9, 9); //フィルタサイズの指定 
+            Cv2.GaussianBlur(img, res, ksize, 0);
+        }
+        //Median フィルタ 
+        private void MedianBlur()
+        {
+            Cv2.MedianBlur(img, res, 15);
+        }
+        //Canny 
+        private void Canny()
+        {
+            Gray();
+            dst = res.Clone();
+            Cv2.Canny(dst, res, 100, 200);
+        }
+        //顔認識
+        private void Kao()
+        {
+            res = img.Clone();
+            Mat gray = new Mat(img.Rows, img.Cols, MatType.CV_8UC1);
+            Cv2.CvtColor(img, gray, ColorConversionCodes.RGB2GRAY);
+            Cv2.EqualizeHist(gray, gray);
+            Rect[] faces = cascade.DetectMultiScale(gray);
+            for (int i = 0; i < faces.Length; i++)
+            {
+                Cv2.Rectangle(res,
+                    new OpenCvSharp.Point(faces[i].X, faces[i].Y),
+                     new OpenCvSharp.Point(faces[i].X + faces[i].Width, faces[i].Y + faces[i].Height),
+                      new Scalar(0, 200, 0)
+                      );
+            }
+        }
+    }    
+}
+
